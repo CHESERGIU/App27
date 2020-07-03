acb78cc54df8d302d982e1481258c60d6acc1636
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:49:52 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace App27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 0;
+            string input;
+            do
+            {                
+                input = Console.ReadLine();
+                int.TryParse(input, out int num);
+                if (num >= 0 && input != "x")
+                    p++;
+            } while (!(input == "x"));
+            Console.WriteLine(p);
+        }
+    }
+}
